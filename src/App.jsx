6180a75e5fcaf31898f697542e8ae9210a1c5610
Vigneshk5 <vigneshpatali5@gmail.com>
2023-02-18 21:49:01 +0530

    firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import firebase from "firebase/app";
 import "firebase/firestore";
@@ -12,8 +12,8 @@ firebase.initializeApp({
   projectId: "chatapp-37c06",
   storageBucket: "chatapp-37c06.appspot.com",
   messagingSenderId: "46358419361",
-  appId: "1:46358419361:web:5b1857a4af565f9f719de0",
-  measurementId: "G-SG7QRH67T8",
+  appId: "1:46358419361:web:bcef797c6c6d831b719de0",
+  measurementId: "G-33VTTCSHCQ",
 });
 
 const auth = firebase.auth();
@@ -25,7 +25,7 @@ function App() {
     <div>
       <div>
         {messages &&
-          message.msp((msg) => <ChatRoom key={msg.id} message={msg} />)}
+          messages.msp((msg) => <ChatRoom key={msg.id} message={msg} />)}
       </div>
       <header></header>
       <section>{user ? <ChatRoom /> : <SignIn />}</section>
@@ -51,11 +51,45 @@ function ChatRoom() {
   const messagesRef = firestore.collection("message");
   const query = messagesRef.orderBy("createdAt").limit(25);
   const [message] = useCollectionData(query, { inField: id });
+  const [formValue, setFormValue] = useState("");
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser();
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimeStamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+  };
+  return (
+    <>
+      <div>
+        {message &&
+          message.msp((msg) => <ChatRoom key={msg.id} message={msg} />)}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={setFormValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">🕊️</button>
+      </form>
+    </>
+  );
 }
 
 function ChatMessage(props) {
-  const { text, uid } = props.messages;
-  return <p>{text}</p>;
+  const { text, uid, photoURL } = props.messages;
+  const messageClass = (uid = auth.currentUser ? "send" : "recived");
+  return (
+    <div className={` message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default App;
